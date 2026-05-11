4c3708facb698b2e514d830995f1d5029d171a1a
24372-cell <24372@burnside.school.nz>
2026-05-12 09:56:22 +1200

    structure

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-print("hello, world")
-print('Hi!')
-print("world")
-print("working?")
-#test
+import sqlite3
+
+DATABASE = 'music.db'
+
+def show_songs():
+    try:
+        user_input = float(input('Minimum duration: '))
+    except:
+        print("Invalid input")
+        return 
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+
+        sql = "SELECT title, artist, duration FROM Music WHERE duration > ?;"
+        cursor.execute(sql, (user_input,))
+
+        results = cursor.fetchall()
+
+        for song in results:
+            print(f"{song[0]} by {song[1]} - {song[2]} min")
+
+if __name__ == "__main__":
+    show_songs()
+

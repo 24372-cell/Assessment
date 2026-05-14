5143359aafd866cb55f6f61309c2390fb8ee0f11
24372-cell <24372@burnside.school.nz>
2026-05-14 12:41:29 +1200

    Add artist

--- app.py
+++ app.py
@@ -19,18 +19,9 @@ def show_songs():
         for song in results:
             print(f"{song[0]} by {song[1]} - {song[2]} min")
 
-if __name__ == "__main__":
-    show_songs()
-
 
 def show_songs():
-    try:
-        genre = input('Enter genre: ')
-    except:
-        print("Invalid input")
-        return 
-    with sqlite3.connect(DATABASE) as db:
-        cursor = db.cursor()
+    genre = input('Enter genre: ')
 
         sql = "SELECT * FROM Music WHERE genre = ?;"
         cursor.execute(sql, (genre,))
@@ -41,7 +32,23 @@ def show_songs():
             print("No songs found.")
         else:
             for song in results:
-                print(song)
+                print(f"{song[1]} by {song[2]} - {song[5]} min")
+
+
+def show_songs():
+    artist = input('Enter artist: ')
+
+        sql = "SELECT * FROM Music WHERE artist = ?;"
+        cursor.execute(sql, (artist,))
+
+        results = cursor.fetchall()
+
+        if not results:
+            print("No songs found.")
+        else:
+            for song in results:
+                print(f"{song[1]} by {song[2]} - {song[5]} min")
 
 if __name__ == "__main__":
     show_songs()
+    

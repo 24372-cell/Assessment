4d747875a9ddc841f53e22c0ab9de3ca92bddfed
24372-cell <24372@burnside.school.nz>
2026-05-18 12:54:56 +1200

    Menu

--- app.py
+++ app.py
@@ -3,51 +3,45 @@ import sqlite3
 DATABASE = 'music.db'
 
 def show_songs():
-    try:
-        user_input = float(input('Minimum duration: '))
-    except:
-        print("Invalid input")
-        return 
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
 
-        sql = "SELECT * FROM Music WHERE duration > ?;"
-        cursor.execute(sql, (user_input,))
+        while True:
+            print("1. Search by genre")
+            print("2. Search by artist")
+            print("3. Search by duration")
+            print("4. Exit")
 
-        results = cursor.fetchall()
+            choice = input("Choose option: ")
+            
+            if choice == "1":
+                genre = input("Enter genre: ")
+                sql = "SELECT * FROM Music WHERE genre = ?;"
+                values = (genre,)
 
-        for song in results:
-            print(f"{song[0]} by {song[1]} - {song[2]} min")
+            elif choice == "2":
+                 artist = input('Enter artist: ')
+                sql = "SELECT * FROM Music WHERE artist = ?;"
+                values = (artist,)
+            
+            elif choice == "3"
+                try:
+                    duration = float(input('Minimum duration: '))
+                except:
+                    print("Invalid input")
+                    continue 
+                sql = "SELECT * FROM Music WHERE duration > ?;"
+                values = (duration,)
+            
+            cursor.execute(sql, values)
+            results = cursor.fetchall()
 
+            if not results:
+                print("No songs found.")
+            else:
+                for song in results:
+                    print(f"{song[0]} by {song[1]} - {song[2]} min")
 
-def show_songs():
-    genre = input('Enter genre: ')
-
-        sql = "SELECT * FROM Music WHERE genre = ?;"
-        cursor.execute(sql, (genre,))
-
-        results = cursor.fetchall()
-
-        if not results:
-            print("No songs found.")
-        else:
-            for song in results:
-                print(f"{song[1]} by {song[2]} - {song[5]} min")
-
-
-def show_songs():
-    artist = input('Enter artist: ')
-
-        sql = "SELECT * FROM Music WHERE artist = ?;"
-        cursor.execute(sql, (artist,))
-
-        results = cursor.fetchall()
-
-        if not results:
-            print("No songs found.")
-        else:
-            for song in results:
-                print(f"{song[1]} by {song[2]} - {song[5]} min")
 
 if __name__ == "__main__":
     show_songs()
